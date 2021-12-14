d38cb9462b24a29381d1dcec770c80edb2792ab4
Simone Morettini <morettinisimo96@gmail.com>
2021-12-14 11:43:35 +0100

    Update plot_matplotlib.py

--- Python/Plots/plot_matplotlib.py
+++ Python/Plots/plot_matplotlib.py
@@ -1,5 +1,25 @@
+################################
+# HISTOGRAM
+# Package needed: matplotlib
+################################
 import matplotlib.pyplot as plt
-
-### HISTOGRAM
 data=[1,2,3,4]
 plt.hist(data, color = 'blue', edgecolor = 'black')
+
+################################
+# BOXPLOT
+# Package needed: numpy matplotlib pandas pyqt5
+#################################
+
+# import the required library 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+ 
+  
+# load the dataset
+df = pd.read_csv("data")
+
+df.boxplot(by ='label', column =['dist'], grid = False)
+
+plt.show()
